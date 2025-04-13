766f955c3932558b846fda02ef37a8af9989834c
Roderick van Domburg <roderick@vandomburg.net>
2025-04-13 16:19:44 +0200

    bench: add benchmarks for audio math operations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,12 +5,17 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
 and [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/).
 
+## [Unreleased]
+
+### Added
+- [benches] Add benchmarks for audio math operations
+
 ## [0.15.0] - 2025-04-10
 
-## Added
+### Added
 - [player] `--max-ram` command-line option to store audio files in RAM instead of disk (#65)
 
-## Changed
+### Changed
 - [player] Preload next track only when nearing the end of the current track
 
 ## [0.14.2] - 2025-04-06
---
+++ benches/audio_math.rs
@@ -0,0 +1,37 @@
+use criterion::{BenchmarkId, Criterion, black_box, criterion_group, criterion_main};
+
+const LN_10: f32 = 2.302585092994046;
+const LN_10_INV: f32 = 0.4342944819032518;
+
+fn bench_logs(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Logarithm");
+    for ratio in [0.125f32, 0.25, 0.5, 0.707, 1.0].iter() {
+        group.bench_with_input(BenchmarkId::new("log10", ratio), ratio, |b, i| {
+            b.iter(|| black_box(*i).log10())
+        });
+        group.bench_with_input(BenchmarkId::new("log_base_10", ratio), ratio, |b, i| {
+            b.iter(|| black_box(*i).log(10.0))
+        });
+        group.bench_with_input(BenchmarkId::new("ln_convert", ratio), ratio, |b, i| {
+            b.iter(|| black_box(*i).ln() * LN_10_INV)
+        });
+    }
+    group.finish();
+}
+
+fn bench_pow(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Power");
+    for db in [-18.0f32, -12.0, -6.0, -3.0, 0.0].iter() {
+        let x = db * 0.05;
+        group.bench_with_input(BenchmarkId::new("powf", db), &x, |b, i| {
+            b.iter(|| 10.0f32.powf(black_box(*i)))
+        });
+        group.bench_with_input(BenchmarkId::new("exp_convert", db), &x, |b, i| {
+            b.iter(|| f32::exp(black_box(*i) * LN_10))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_logs, bench_pow);
+criterion_main!(benches);
